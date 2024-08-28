6ef0351ffc5a46466c36da6efdac68ac31ad2b08
Michał Sikorski <michalsikorski61@gmail.com>
2024-08-28 23:45:08 +0200

    py variables and print

---
+++ README.md
@@ -0,0 +1,4 @@
+cou - cours of any kind
+bok - book of any kind
+tut - tutorial of any kind
+btw - side trials
---
+++ tut_python_od_podstaw_2024/first_script.py
@@ -0,0 +1,5 @@
+# print("Cześć Michał") # print text to console
+name = "Michał" # assign value to variable, variable name is case sensitive, variable is like box with label (in memory location)
+print("Cześć", name) # print text and variable to console
+print("Cześć " + name + "!") # print text and variable to console, concatenate strings
+print(f"Cześć {name}!") # print text and variable to console, f-string
