0bd8c5bfef0ec36e139edeb29b9417b63a5d7878
Michał Sikorski <michalsikorski61@gmail.com>
2024-08-29 17:32:28 +0200

    dynamic language - Python

---
+++ tut_python_od_podstaw_2024/10_dynamic_strong_type_lang.py
@@ -0,0 +1,36 @@
+def add(a, b):
+    print(a + b)
+
+a = 'michal' # dynamic typing that allows to change the type of the variable, strong typing that does not allow to mix types
+b = 'kowalski' # dynamic typing that allows to change the type of the variable, strong typing that does not allow to mix types
+a = 2 # dynamic typing that allows to change the type of the variable, strong typing that does not allow to mix types
+b = 3 # dynamic typing that allows to change the type of the variable, strong typing that does not allow to mix types
+add(a, b) # 5
+
+a = 3
+b = 'michal'
+# add(a, b) # TypeError: unsupported operand type(s) for +: 'int' and 'str'
+# print(a + b) # TypeError: unsupported operand type(s) for +: 'int' and 'str'
+
+print('---------------------')
+a= 3
+b = '3'
+print(type(a)) # <class 'int'>
+print(type(b)) # <class 'str'>
+print(a + int(b)) # 6
+
+print(type(a)) # <class 'str'>
+print(type(b)) # <class 'str'>
+
+print('---------------------')
+c = 'Michal'
+print(type(c)) # <class 'str'>
+c = 3
+print(type(c)) # <class 'int'>
+c = 3.0
+print(type(c)) # <class 'float'>
+c = True
+print(type(c)) # <class 'bool'>
+c = None
+print(type(c)) # <class 'NoneType'>
+# we can change the type of the variable in Python
