3517dfff4a16292019546f17daebd3f18767c24a
Michał Sikorski <michalsikorski61@gmail.com>
2024-08-29 20:30:55 +0200

    area of rectangle

---
+++ tut_python_od_podstaw_2024/15_7_exercise_7.py
@@ -0,0 +1,7 @@
+# area of a rectangle
+# area = width * height
+
+width = int(input('Enter the width: '))
+height = int(input('Enter the height: '))
+area = width * height
+print(f'The area of the rectangle is {area}')
