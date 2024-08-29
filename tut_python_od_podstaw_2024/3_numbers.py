1e3c475b5008dd459b8b1c145d828d8f1b4a1d95
Michał Sikorski <michalsikorski61@gmail.com>
2024-08-29 10:50:03 +0200

    booleans

---
+++ tut_python_od_podstaw_2024/3_numbers.py
@@ -0,0 +1,13 @@
+a = 5.1
+b = 2.2
+
+print(a + b) # 7
+print(a - b) # 3
+print(a * b) # 10
+print(a / b) # 2.5
+print(a // b) # 2
+print(a % b) # 0.7
+
+# a++ # SyntaxError: invalid syntax
+a += 1
+print(a)
