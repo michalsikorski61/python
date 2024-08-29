fea82dabc430a8f8e678b53dac543196d9823c27
Michał Sikorski <michalsikorski61@gmail.com>
2024-08-29 20:25:11 +0200

    prime nums 1-100

---
+++ tut_python_od_podstaw_2024/15_5_exercise_5.py
@@ -0,0 +1,12 @@
+# liczby pierwsze
+# Napisz program, który wyświetli wszystkie liczby pierwsze z zakresu od 1 do 100.
+#
+# Podpowiedź: liczba pierwsza to taka liczba, która dzieli się tylko przez 1 i samą siebie.
+for i in range(1, 101):
+    if i < 2:
+        continue
+    for j in range(2, i):
+        if i % j == 0:
+            break
+    else:
+        print(i)
