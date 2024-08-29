a88cafb81c4c76939c1c2555e5830ceb4a8ea302
Michał Sikorski <michalsikorski61@gmail.com>
2024-08-29 20:51:00 +0200

    man or woman

---
+++ tut_python_od_podstaw_2024/15_8_exercise_7.py
@@ -0,0 +1,6 @@
+# is name of man or woman
+name = input("Jak masz na imię? ") # get input from user
+if name.endswith('a'): # if statement, condition
+    print("Jesteś kobietą.") # print text to console
+else:
+    print("Jesteś mężczyzną.")
