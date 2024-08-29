fee70f608b9fea3cc0317aec2fe2a4357f8d4e3d
Michał Sikorski <michalsikorski61@gmail.com>
2024-08-29 18:50:52 +0200

    loop while

---
+++ tut_python_od_podstaw_2024/13_loops_while.py
@@ -0,0 +1,12 @@
+#while loop
+i = 1
+while i< 5: # 1, 2, 3, 4 loop is a block of code that is executed multiple times
+    print(i)
+    i += 1
+
+name  = None # None is a special value in Python that represents the absence of a value
+print('---------------------')
+while name != 'Michal':
+    name = input('Enter your name: ')
+
+print("Hi Michal!")
