15cfdb812958c543d49ae16c51d5a264b06b1104
Michał Sikorski <michalsikorski61@gmail.com>
2024-08-30 08:30:24 +0200

    Countries and capitals dict

---
+++ tut_python_od_podstaw_2024/19_6_exercise.py
@@ -0,0 +1,11 @@
+#dict with countries and their capitals
+countries = {
+    'Poland': 'Warsaw',
+    'Germany': 'Berlin',
+    'France': 'Paris',
+    'Spain': 'Madrid',
+    'Italy': 'Rome',
+}
+
+for country, capital in countries.items():
+    print(f' {country} - {capital}.')
