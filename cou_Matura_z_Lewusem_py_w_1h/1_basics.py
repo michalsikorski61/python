879ed7831ff54b2ab5631e3ce3b5362fc98bddbb
Michał Sikorski <michalsikorski61@gmail.com>
2024-08-30 16:26:36 +0200

    ifs

--- cou_Matura_z_Lewusem_py_w_1h/1_basics.py
+++ cou_Matura_z_Lewusem_py_w_1h/1_basics.py
@@ -1,17 +1,8 @@
-bank_account = int(input("Podaj stan konta: "))
-bank_account += 500*2
-print(bank_account)
-
-# int - integer - whole number
-# float - floating-point number
-# str - string - sequence of characters
-# bool - boolean - logical value
-# list - list
-# char - character
-
-x = 9
-y = x//2
-print(y)
-x **=2 
-x **= (1/2) # x = x ** (1/2) - square root
-print(x)
+temp = -2
+is_happy = False
+if temp > 10 or is_happy:
+    print(f"Go out, it's {temp} degrees")
+elif temp > -10 and  not is_happy:
+    print(f"Put on a jacket, it's {temp} degrees")
+else:
+    print(f"Stay home, it's {temp} degrees")
