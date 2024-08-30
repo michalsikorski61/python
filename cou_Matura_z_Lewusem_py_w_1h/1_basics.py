1f28493e6272a519f4ae50e6afa0359c9fa51cc4
Michał Sikorski <michalsikorski61@gmail.com>
2024-08-30 16:56:23 +0200

    loops

--- cou_Matura_z_Lewusem_py_w_1h/1_basics.py
+++ cou_Matura_z_Lewusem_py_w_1h/1_basics.py
@@ -1,8 +1,24 @@
-temp = -2
-is_happy = False
-if temp > 10 or is_happy:
-    print(f"Go out, it's {temp} degrees")
-elif temp > -10 and  not is_happy:
-    print(f"Put on a jacket, it's {temp} degrees")
-else:
-    print(f"Stay home, it's {temp} degrees")
+# for i in range(10):
+#     print('Hello, World!')
+
+for i in range(1,10): # range(1,10) returns a sequence of numbers from 1 to 9, 10 is excluded
+    if i != 5:
+        print(i)
+
+for i in range(1,10,2): # range(1,10,2) returns a sequence of numbers from 1 to 9 with a step of 2
+    if i == 5:
+        break
+    print(i)
+
+print('-----------------')
+
+temperture = 20
+while temperture < 25:
+    print('Temperture is too low')
+    temperture += 1
+
+print('-----------------')
+for i in range(1,10):
+    if i == 5:
+        continue
+    print(i)
