7a5c5426dda2f7b736b6ebb0fe56073372c9373c
Michał Sikorski <michalsikorski61@gmail.com>
2024-08-30 08:34:04 +0200

    sort dict

---
+++ tut_python_od_podstaw_2024/19_7_sort_dict.py
@@ -0,0 +1,12 @@
+#dict with countries and their capitals
+countries = {
+    'Poland': 'Warsaw',
+    'Germany': 'Berlin',
+    'France': 'Paris',
+    'Spain': 'Madrid',
+    'Italy': 'Rome',
+}
+
+#sort by country
+countries = dict(sorted(countries.items()))
+print(countries)
