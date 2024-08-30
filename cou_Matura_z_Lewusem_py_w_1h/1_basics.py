5920f4efc80e1b3dac9c825fc8bfcc5d55137224
Michał Sikorski <michalsikorski61@gmail.com>
2024-08-30 20:57:48 +0200

    sets

--- cou_Matura_z_Lewusem_py_w_1h/1_basics.py
+++ cou_Matura_z_Lewusem_py_w_1h/1_basics.py
@@ -1,63 +1,21 @@
-
-from copy import deepcopy
-grades = [2, 3, 4, 5, 6]
-print(grades[0])  # 2
-print(grades[1])  # 3
-print(grades[2])  # 4
-
-for grade in grades:
-    print(grade)
-
-print('---')
-for i in range(len(grades)):
-    print(grades[i],end=' ')
-
-print()
-print('---')
-for i,grade in enumerate(grades):
-    print(i,grade)
-
-print('---')
-for i,grade in enumerate(grades):
-    grades[i] = grade + 1
-
-grades.append(6)
-grades.extend([7,8])# we must give a list or any iterable object
-grades.insert(0,1) # we must give an index and a value
-print(grades)
-# how to find an index of a value
-grades.remove(6) # we must give a value
-print(grades)
-print(grades.pop()) # 8
-print(grades)
-grades.sort() # or sorted(grades)
-print(grades) 
-grades.sort(reverse=True) # or sorted(grades,reverse=True)
-print(grades)
-print(grades.index(5)) # 3
+# set don't allow duplicates
+# set is unordered
+# set is mutable
+# set is iterable
+grades = {1, 2, 3, 4, 5, 5, 5, 5, 5, 5}
+grades_list = [1, 2, 3, 4, 5, 5, 5, 5, 5, 5]
+print(grades_list)
+grades_set = set(grades_list)
+print(grades_set)
 
 print('-----------------')
+marks_grades = [1, 2, 3, 4, 5, 5, 5, 5, 5, 5]
+anne_grades = [1,2,1,2,3,1,1,2,2,2]
+# grades that are in both lists
+marek_set = set(marks_grades)
+anne_set = set(anne_grades)
+print(marek_set.intersection(anne_set)) # {1, 2, 3} # grades that are the same in both lists
+print(marek_set.union(anne_set)) # {1, 2, 3, 4, 5} # grades that are in both lists
+print(marek_set.difference(anne_set)) # {4, 5} # grades that are in first list but not in second list
+print(anne_set.difference(marek_set)) # {3} # grades that are in second list but not in first list
 
-if 7 in grades:
-    print('7 is in grades')
-
-grades_all =  [
-    [2,3,4,5,6],
-    [3,4,5,6,7],
-    [4,5,6,7,8],
-]
-print(grades_all[0])
-print(grades_all[0][0])
-
-print('-----------------')
-for grades in grades_all:
-    print(grades,end=' ')
-    for grade in grades:
-        print(grade,end=' ')
-    print()
-
-print('-----------------')
-grades_2 = grades_all # we have two references to the same object, so we can change the object through both references
-grades_2[0][0] = 1 # we change the object grades_all, not grades_2
-print(grades_all)
-grades_2 = deepcopy(grades_all) # we have two different objects
