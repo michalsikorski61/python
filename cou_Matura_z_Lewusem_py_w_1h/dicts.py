e972d2f1f7d2ff138e9b2d2ac03c63366d5c1171
Michał Sikorski <michalsikorski61@gmail.com>
2024-09-12 15:15:29 +0200

    Lewus dicts

---
+++ cou_Matura_z_Lewusem_py_w_1h/dicts.py
@@ -0,0 +1,8 @@
+marks_dict ={}
+marks_dict['marek'] = [1, 2, 3, 4, 5, 5, 5, 5, 5, 5]
+marks_dict['anne'] = [1,2,1,2,3,1,1,2,2,2]
+marks_dict['kozak'] = [1,2,1,2,3,1,1,2,2,2]
+print(marks_dict)
+
+for key,value in marks_dict.items():
+    print(key, value)
