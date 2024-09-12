780c2aa0b5bedc89f42cc8a30acf432e7868840e
Michał Sikorski <michalsikorski61@gmail.com>
2024-09-12 23:32:49 +0200

    Lewus builtin fns

---
+++ cou_Matura_z_Lewusem_py_w_1h/6_builtin_fns.py
@@ -0,0 +1,21 @@
+import math
+
+print(math.gcd(24,36)) # largest common divisor
+
+# arrays fns
+marks = [1, 2, 3, 4, 5, 5, 5, 5, 5, 5]
+print("Sum of marks:", sum(marks))
+print("Maximum mark:", max(marks))
+print("Minimum mark:", min(marks))
+
+# sqrt
+print("Square root of 16:", math.sqrt(16))
+# round
+print("3.14159265359 rounded to 2 decimal places:", round(3.14159265359, 2))
+# ceil
+print("Ceiling of 3.1:", math.ceil(3.1))
+# floor
+print("Floor of 3.9:", math.floor(3.9))
+
+# abs
+print("Absolute value of -3:", abs(-3))
