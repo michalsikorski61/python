a407134fac37952e6f9dd0d0851eca11f7626fc8
Michał Sikorski <michalsikorski61@gmail.com>
2024-09-12 23:40:19 +0200

    2 noob habit manually close file 2

--- 25_nooby_py_habits/2_manual_closing_file.py
+++ 25_nooby_py_habits/2_manual_closing_file.py
@@ -9,3 +9,12 @@ def manually_calling_close_on_a_file(filename):
         f.write('Hello\n')
 
 #  ensure the file is closed even if an exception occurs
+
+# you can also use the finally block to ensure the file is closed
+def finally_instead_of_context_manager(host,port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.connect((host,port))
+        s.sendall(b'Hello, world')
+    finally:
+        s.close()
