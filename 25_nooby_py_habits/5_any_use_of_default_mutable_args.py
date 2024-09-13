1b78646ceb22df96a1374bb49588f722f8f133c5
Michał Sikorski <michalsikorski61@gmail.com>
2024-09-13 02:14:56 +0200

    noob habit any use of def mutable args

---
+++ 25_nooby_py_habits/5_any_use_of_default_mutable_args.py
@@ -0,0 +1,11 @@
+def append(n,l=[]):
+    l.append(n)
+    return l
+
+l1 = append(0) # [0]
+l2 = append(1) # [0,1] oops ,should be [1]
+
+
+# args default are defined when the fn is defined, not when it's called
+# so the default mutable args are shared between calls, in this case the list
+# is shared between calls to append
