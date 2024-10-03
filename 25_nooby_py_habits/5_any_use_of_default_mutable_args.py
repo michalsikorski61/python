a3af3aadc6d6d14f6d5fe66ba7e3640b9e39c0fc
Michał Sikorski <michalsikorski61@gmail.com>
2024-10-03 08:10:26 +0200

    noob habit def mutable args fixed ver II meets expectation

--- 25_nooby_py_habits/5_any_use_of_default_mutable_args.py
+++ 25_nooby_py_habits/5_any_use_of_default_mutable_args.py
@@ -9,3 +9,13 @@ l2 = append(1) # [0,1] oops ,should be [1]
 # args default are defined when the fn is defined, not when it's called
 # so the default mutable args are shared between calls, in this case the list
 # is shared between calls to append
+
+#right way to do it
+def append(n,l=None):
+    if l is None:
+        l = []
+    l.append(n)
+    return l
+
+l1 = append(0) # [0]
+l2 = append(1) # [1]
