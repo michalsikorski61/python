d88a8fbae9a467e97ca7eaa612d85c0c878d7f4e
Michał Sikorski <michalsikorski61@gmail.com>
2025-01-28 20:12:35 +0100

    KoW py basics

---
+++ KoW_kurs_python/1_wstep/main.py
@@ -0,0 +1,18 @@
+print('jakieś pierdoły', 5 +3 )
+print('to będzie', end=' ')
+print('wszystko w jednej linii')
+
+print('dodwanie ',5+7)
+print('odejmowanie', 5-7)
+print('mnożenie', 5*7)
+print('dzielenie', 6/7)
+print('modulo (reszta z dzielenia)', 6 % 7)
+print('dzielenie całkowite', 6//7)
+print('potęgowanie', 6**7)
+
+print('hello' + 'world')
+print(' tu będzie słowo kilka razy: {}'.format('world ' * 3))
+
+#inna cześć kodu
+zmienna1 = input('podaj rzecz dyamicznie typowaną: ')
+print('Podałeś: {}'.format(zmienna1))
