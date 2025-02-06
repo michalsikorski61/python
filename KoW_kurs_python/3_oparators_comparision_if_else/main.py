c04e302152a398abb56815ff0190c2f03c70187c
Michał Sikorski <michalsikorski61@gmail.com>
2025-02-06 09:00:45 +0100

    KoW comparision and if

---
+++ KoW_kurs_python/3_oparators_comparision_if_else/main.py
@@ -0,0 +1,26 @@
+x = True
+y = False
+print(x, y)
+print( 5 == 5) # is equal?
+print( 5 != 5) # is not equal?
+print( 5 > 5) # is greater?
+print(5 < 5) # is less?
+print(5 >= 5) # is greater or equal?
+print(5 <= 5) # is less or equal?
+
+print(' ---------------------------------- ')
+if 5 == 5:
+    print('Condition is True')
+else:
+    print('Condition is False')
+
+print('Program continues')
+
+if 5 < 10:
+    print('True')
+elif 2 == 2:
+    print('Another True')
+else:
+    print('Nothing is True')
+
+print(' ---------------------------------- ')
