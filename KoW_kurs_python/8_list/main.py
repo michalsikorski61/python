1cba98d0b4386faa20106d2cc2db2c60b0d324fe
Michał Sikorski <michalsikorski61@gmail.com>
2025-02-13 07:57:57 +0100

    KoW list2

---
+++ KoW_kurs_python/8_list/main.py
@@ -0,0 +1,19 @@
+lista = ["Sub", "Kanał", "o", "Wszystkim"]
+
+
+# i = 0
+# while i < len(lista):
+#     print(lista[i],end=' ')
+#     i += 1
+
+for x in lista:
+    print(x,end=' ')
+
+print()
+print(list(range(10)))
+print(type(range(10)))
+
+print("------------------------------")
+ 
+for y in range(2,10,2):
+    print(y,end='-')
