9de0e79be17dc8eb1091b7f34df00ffa638ccb5a
Michał Sikorski <michalsikorski61@gmail.com>
2025-04-13 19:12:31 +0200

    py mosh slicing

--- cou python full course for beginners [2025] programming with Mosh/start.py
+++ cou python full course for beginners [2025] programming with Mosh/start.py
@@ -1,1 +1,14 @@
-print("Hello World ❤️")
+course = "Python Programming"
+print(len(course))  # 20
+print(course[0])  # P
+print(course[-1])  # g
+print(course[0:3])  # Pyt
+print(course[0:])  # Python Programming
+print(course[:3])  # Pyt
+print(course[1:8])  # hon Pro
+print(course[1:8:2])  # h P
+print(course[::2])  # Pto rgamn
+print(course[::-1])  # gnimmargorP nohty
+print(course[::-2])  # gmr oP
+print(course[1:8:2])  # h P
+print(course[1:8:3])  # hP
