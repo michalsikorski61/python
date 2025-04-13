f9c5bec529d5b886fde98d3054090ad8c9ad84e8
Michał Sikorski <michalsikorski61@gmail.com>
2025-04-13 19:21:08 +0200

    py Mosh str mth

--- cou python full course for beginners [2025] programming with Mosh/start.py
+++ cou python full course for beginners [2025] programming with Mosh/start.py
@@ -1,4 +1,12 @@
-first = "Michał"
-last = "Kowalski"
-full = f"{first} {last} {2 + 2}"
-print(full)
+course = "python programming"
+print(course.strip())
+print(course[0:3])  # pyt
+# rstrip
+print(course.rstrip())  # python programming
+# find
+print(course.find("Beginners"))  # 7
+print(course.find("Beginnerss"))  # -1
+# replace
+print(course.replace("p", "j"))  # jython programming
+print("pro" in course)  # True
+print("swift" not in course)  # False
